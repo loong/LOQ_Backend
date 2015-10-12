e6c73601e6b2f0c20a63da0a647e31bef8991be1
Long Hoang <long@mindworker.de>
2015-10-13 00:28:51 +0800

    LH: Implemented barebone image upload feature. Still need beautification and generalisation!

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-var express = require('express')
-var app = express()
+//////////////////////////////////////////////////////////////////////
+///
+/// @brief Backend system for interactive in-class 'forum'
+///
+/// @author Hong Joon Choi
+/// @author Long Hoang <long@mindworker.de>
+///
+//////////////////////////////////////////////////////////////////////
 
-app.set('port', (process.env.PORT || 5000))
+var express     =   require("express");
+var multer      =   require('multer');
+var app         =   express();
+
+
+//////////////////////////////////////////////////////////////////////
+/// Image upload
+//////////////////////////////////////////////////////////////////////
+var upload      =   multer({ dest: './uploads/'});
+
+app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename+Date.now();               // Ensures uniqueness
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file, req) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+
+	// Use dependency injection to pass on this variable
+	req.filepath = file.path;
+    }
+}));
+
+/// @todo return JSON object with filepath
+app.post('/api/uploadphoto', function(req, res){
+    upload(req, res, function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded\n"+req.filepath);
+    });
+});
+
+
+//////////////////////////////////////////////////////////////////////
+/// General Stuff
+//////////////////////////////////////////////////////////////////////
+
+app.get('/',function(req,res){
+      res.end("Hello World");
+});
+
+// Static fileserver serving files in /public folder
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen((process.env.PORT || 5000), function(){
+    console.log("Working on port 3000");
+});
