ab54b1db3eb7e9f2071bbd5a8129c6500170d0d9
Sonu Sarda <Sonussarda@Sonus-MacBook-Air-3.local>
2015-10-22 12:02:53 +0800

    Merged mongoDB with image upload

---
+++ app/models/question.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+	id: Number,
+	userId: Number,
+	text: String,
+	imageURL: String,
+	time: {type: Date, default: Date.now},
+	answers: [{
+		id: Number,
+		userId: Number,
+		username: String,
+		text: String,
+		time: Date,
+		imageURL: String,
+		follow_ups: [{
+			id: Number,
+			userId: Number,
+			username: String,
+			text: String,
+			time: Date,
+			imageURL: String
+		}]
+	}]
+});
+
+
+// {
+//     "id": 1,
+//     "userId": 2,
+//     "text": "Question 1 Sample",
+//     "imageURL": "google.com",
+//     "answers":[{
+//         "username":"IwillSaveYou",
+//         "id":2,
+//         "text":"Dude ofcourse the answer is just this",
+//         "imageURL":"google.com",
+//         "follow_ups": [
+//             {
+//                 "username": "followUpDude",
+//                 "text":"no way man"
+//             },
+//             { 
+//                 "username": "followUpYourfollowUP",
+//                 "text":"YES WAY!"
+//             }
+//         ]
+//     }]
+// }
+
+
+// exports this model and make sit available to server.js
+// note that when making a model of Question, mongodb makes collection questions
+// automatically plural
+module.exports = mongoose.model('Question', QuestionSchema)
+
