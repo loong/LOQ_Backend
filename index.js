d30091805bbd16383b4cd4b81378d8135b05be53
Sonu Sarda <Sonussarda@Sonus-MacBook-Air-3.local>
2015-10-23 22:10:25 +0800

    merged image upload with routing

--- index.js
+++ index.js
@@ -2,19 +2,124 @@
 ///
 /// @brief Backend system for interactive in-class 'forum'
 ///
+/// @author Amogh Sarda
 /// @author Hong Joon Choi
 /// @author Long Hoang <long@mindworker.de>
 ///
 //////////////////////////////////////////////////////////////////////
 
-var express     =   require("express");
-var multer      =   require('multer');
-var app         =   express();
+
+
+//////////////////////////////////////////////////////////////////////
+/// Routing and middleware
+//////////////////////////////////////////////////////////////////////
+
+// setup express
+var express = require('express');
+var app = express();
+
+//need body parser for POST requests (req parser)
+var bodyParser = require('body-parser');
+
+// setup mongo and connect
+// db used is sample
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/sample';
+mongoose.connect(url);
+
+// refer to Question model (schema)
+var Question = require('./app/models/question');
+
+// use url encoded parsing. may need to change this.
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// establish the routes
+var router = express.Router();
+
+//middleware for any pre-processing of requests (sanitising?)
+router.use(function(req, res, next) {
+    console.log("Processing REST API request");
+    next();
+});
+
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the API'});
+});
+
+// POST add question
+// GET show all questions
+router.route('/question')
+    
+    .post(function(req, res) {
+        var question = new Question(req.body);
+        console.log(JSON.stringify(req.body));
+        //question.text = req.body.text;
+
+        question.save(function(err) {
+            if (err)
+                res.send(err)
+
+            res.json({ message: 'Added Question!'});
+
+        });
+    })
+
+    .get(function(req, res) {
+        Question.find(function(err, questions){
+            if (err)
+                res.send(err);
+            res.json(questions);
+        });
+    });
+
+
+// GET question by id
+// DELETE question by id
+router.route('/question/:question_id')
+    .get(function(req, res) {
+        Question.findById(req.params.question_id,  function(err, question) {
+            if (err)
+                res.send(err);
+            res.json(question);
+        });
+    })
+
+    .delete(function(req, res) {
+        Question.remove({
+            _id: req.params.question_id
+        }, function(err, question) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+router.route('/answer/:question_id')
+    .post(function(req, res) {
+
+    })
+
+// root to /api/v1
+app.use('/api/v1', router);
+
+app.listen(port);
+console.log("Okay starting node server");
+
+
 
 //////////////////////////////////////////////////////////////////////
 /// Image upload
 //////////////////////////////////////////////////////////////////////
+var multer      =   require('multer');
 var upload      =   multer({ dest: './public/uploads/'});
+
+// Static fileserver serving files in /public folder
+app.use(express.static(__dirname + '/public'))
 
 app.use(multer({ dest: './public/uploads/',
     rename: function (fieldname, filename) {
@@ -28,8 +133,8 @@ app.use(multer({ dest: './public/uploads/',
     onFileUploadComplete: function (file, req) {
         console.log(file.fieldname + ' uploaded to  ' + file.path)
 
-	// Use dependency injection to pass on this variable
-	req.filepath = file.path.replace("public/", "");
+    // Use dependency injection to pass on this variable
+    req.filepath = file.path.replace("public/", "");
     }
 }));
 
@@ -42,41 +147,3 @@ app.post('/api/uploadphoto', function(req, res){
     });
 });
 
-
-//////////////////////////////////////////////////////////////////////
-/// General Stuff
-//////////////////////////////////////////////////////////////////////
-
-app.get('/',function(req, res){
-      res.end("Hello World");
-});
-
-// Static fileserver serving files in /public folder
-app.use(express.static(__dirname + '/public'))
-
-app.listen((process.env.PORT || 5000), function(){
-    console.log("Working on port 5000");
-});
-
-//////////////////////////////////////////////////////////////////////
-/// For testing purposes. Feel free to remove them anytime
-//////////////////////////////////////////////////////////////////////
-
-var testCounter = 0;
-
-app.get('/testCounter',function(req, res){
-    res.send({err:null, result:testCounter});
-});
-
-app.post('/testCounter',function(req, res){
-    console.log(req.body.counterValue);
-
-    if (req.body.counterValue<0 || req.body.counterValue>1000){
-        res.send({err:"counter request value is out of range", result:null});
-    }
-    else{
-        testCounter = req.body.counterValue;
-        res.send({err:null, result:testCounter});
-    }
-
-});
